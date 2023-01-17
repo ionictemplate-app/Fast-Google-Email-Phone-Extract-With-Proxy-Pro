0e2b2126baafab4ccdcc486cdb6cb108fc5b811b
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2023-01-17 22:20:33 +0800

    Add project files.

---
+++ Fast-Google-Email-Phone-Extract-With-Proxy-Pro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast_Google_Email_Phone_Extract_With_Proxy_Pro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
